35d3d55ec3507167e861126e245eadc0fae66205
Okman-0920 <nohoksun94@gmail.com>
2025-01-01 22:29:42 +0900

    refactoring

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -62,27 +62,29 @@ public class PostController {
         return body;
     }
 
-    @GetMapping("/write")
-    public String showWrite() {
-        return "domain/post/post/write";
-    }
 
     // 다른곳에서 이거 사용 못하게 private
     private record PostWriteForm(
             @NotBlank(message = "01-제목을 입력해주세요.")
-            @Length(min = 5, message = "02-제목을 5자 이상 입력해주세요.")
+            @Length(min = 2, message = "02-제목을 5자 이상 입력해주세요.")
             String title,
             @NotBlank(message = "03-내용을 입력해주세요.")
-            @Length(min = 10, message = "04-내용을 10자 이상 입력해주세요.")
+            @Length(min = 2, message = "04-내용을 10자 이상 입력해주세요.")
             String content
+    ) { }
+
+    @GetMapping("/write")
+    public String showWrite(
+            PostWriteForm form
     ) {
+        return "domain/post/post/write";
     }
 
     @PostMapping("/write")
     public String write(
             @ModelAttribute("form") @Valid PostWriteForm form,
             // 암기: @Model~ : 요청 데이터를 Java객체로 변환하는 어노테이션
-            // 암기: 위처럼 하면 타임리프 form이라는 변수로 가능함
+            // 암기: 위처럼 하면 타임리프 form이라는 변수로 가능함 (생략 가능)
             BindingResult bindingResult,
             // 암기: 사용자의 입력값에 대한 결과를 보관
             Model model
